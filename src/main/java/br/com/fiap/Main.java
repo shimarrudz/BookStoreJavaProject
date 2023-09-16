3b7fa3b60df5c58d0c31cf3e591ebcf5a7b89228
Benefrancis <benefrancis@gmail.com>
2023-09-15 22:12:44 -0300

    Criamos as entidades Pessoa, PessoaFisica e PessoaJuridica e mapeamos

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,46 +1,70 @@
 package br.com.fiap;
 
-import br.com.fiap.domain.entity.Author;
-import br.com.fiap.domain.entity.Book;
+import br.com.fiap.domain.entity.*;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public class Main {
     public static void main(String[] args) {
 
-        EntityManagerFactory factory = Persistence.createEntityManagerFactory("oracle");
+        EntityManagerFactory factory = Persistence.createEntityManagerFactory( "oracle" );
         EntityManager manager = factory.createEntityManager();
 
+        var holding = new PessoaJuridica();
+        var bene = new PessoaFisica();
+        var vinicius = new PessoaFisica();
 
-        var bene = new Author(null, "Benefrancis");
-        var raquel = new Author(null, "Raquel");
-        var guilherme = new Author(null, "Guilherme");
+        holding.setCnpj( "12313213/0001-21" ).setId( null )
+                .setNome( "Holding Benezinho SA" )
+                .setNascimento( LocalDate.now().minusYears( 5 ) );
 
-        var livro = new Book();
 
-        livro.setId(null)
-                .setName("Java Mapeamento Objeto Relacional")
-                .setLaunch(LocalDateTime.now())
-                .setISBN(UUID.randomUUID().toString())
-                .addAuthor(bene)
-                .addAuthor(raquel)
-                .addAuthor(guilherme);
+        bene.setCpf( "213124164" )
+                .setNome( "Benefrancis do Nascimento" )
+                .setNascimento( LocalDate.of( 1977, 3, 8 ) )
+                .setId( null );
 
-        manager.getTransaction().begin();
+        vinicius.setCpf( "131231321" )
+                .setId( null )
+                .setNome( "Vinicius Cruzeiro Barbosa" )
+                .setNascimento( LocalDate.now().minusYears( 17 ) );
 
-        manager.persist(livro);
 
-        manager.getTransaction().commit();
-
+        System.out.println(holding);
+        System.out.println(bene);
+        System.out.println(vinicius);
 
         manager.close();
         factory.close();
 
-        System.out.println(livro);
 
+    }
+
+    private static Book newBook(EntityManager manager) {
+        var bene = new Author( null, "Benefrancis" );
+        var raquel = new Author( null, "Raquel" );
+        var guilherme = new Author( null, "Guilherme" );
+
+        var livro = new Book();
+
+        livro.setId( null )
+                .setName( "Java Mapeamento Objeto Relacional" )
+                .setLaunch( LocalDateTime.now() )
+                .setISBN( UUID.randomUUID().toString() )
+                .addAuthor( bene )
+                .addAuthor( raquel )
+                .addAuthor( guilherme );
+
+        manager.getTransaction().begin();
+
+        manager.persist( livro );
+
+        manager.getTransaction().commit();
+        return livro;
     }
 }
