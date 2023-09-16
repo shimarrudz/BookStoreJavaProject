aee759bbb10175433db4b67cfb9bcd2068f8e159
Benefrancis <benefrancis@gmail.com>
2023-09-15 21:38:19 -0300

    Iniciando a criação da classe Pessoa

---
+++ src/main/java/br/com/fiap/domain/entity/Pessoa.java
@@ -0,0 +1,14 @@
+package br.com.fiap.domain.entity;
+
+import java.time.LocalDate;
+
+public class Pessoa {
+
+    private Long id;
+
+    private String nome;
+
+    private LocalDate nascimento;
+
+
+}
